1974695b686593c36e70797d7ca182580e2cbc36
Hassaan Ali <zfhassaan@gmail.com>
2023-01-04 11:36:27 +0500

    Update JazzCash.php

--- src/JazzCash.php
+++ src/JazzCash.php
@@ -1,24 +1,32 @@
 <?php
 
-namespace zfhassaan\jazzcash;
+namespace Zfhassaan\JazzCash;
 
 use Carbon\Carbon;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use zfhassaan\jazzcash\Gateway\RefundPayment;
 
 class JazzCash
 {
 
-    protected $api_mode;
-    protected $apiUrl;
-    protected $merchant_id;
-    protected $return_url;
-    protected $password;
-    protected $timezone;
+    protected string $api_mode;
+    protected string $apiUrl;
+    protected string $merchant_id;
+    protected string $return_url;
+    protected string $password;
+    protected string $timezone;
 
-//    Post Fields
-    private $amount;
-    private $billreference;
-    private $productdescription;
+    //    Post Fields
+    private int $amount;
+    private string $billreference;
+    private string $productdescription;
+    private string $mpin;
+    // Refund URL
+    protected string $refundURL;
 
     /**
      * Constructor for JazzCash Payment Gateway
@@ -42,11 +50,15 @@ class JazzCash
         $this->return_url = config('jazzcash.return_url');
         $this->password = config('jazzcash.password');
         $this->timezone = date_default_timezone_set('Asia/Karachi');
-
-
+        $this->api_mode === 'sandbox' ? $this->setRefundApiUrl(config('jazzcash.refund_sandbox_url')) : $this->setRefundApiUrl(config('jazzcash.refund_production_url'));
+        $this->mpin = config('jazzcash.mpin');
     }
 
-    public function sendRequest()
+    /**
+     * Send Request
+     * @return Response|Application|ResponseFactory
+     */
+    public function sendRequest(): Response|Application|ResponseFactory
     {
         $data['amount'] = $this->getAmount() * 100;  //Last two digits will be considered as Decimal
         $data['billRef'] = $this->getBillRefernce();
@@ -72,19 +84,58 @@ class JazzCash
     }
 
     /**
+     * Send Refund Request...
+     *
+     */
+    public function RequestRefund($request): JsonResponse
+    {
+        $data['pp_Amount'] = $request['amount'];
+        $data['pp_MerchantID'] = $this->merchant_id;
+        $data['pp_MerchantMPIN'] = $this->mpin;
+        $data['pp_Password'] = $this->password;
+        $data['pp_TxnCurrency'] = 'PKR';
+        $data['pp_TxnRefNo'] = $request['txnref'];
+        $data['pp_SecureHash'] = $this->RefundHashArray($data);
+//        $data['pp_SecureHash'] = "2C595361C2DA0E502D18BFBAA92CF4740330215E5E8AD0CF4489A64E7400B117";
+        $url = $this->getRefundApiUrl();
+        $refund = new RefundPayment();
+        $result = $refund->sendRequest($url, $data);
+        return response()->json($result);
+    }
+
+    /**
+     * Refund Hash Array.
+     */
+    public function RefundHashArray($data)
+    {
+        $result = [];
+        foreach($data as $key => $value)
+        {
+            $result[] = $value;
+        }
+        $resultString = '';
+        foreach($result as $key => $value)
+        {
+            $resultString .= $value.'&';
+        }
+        $SortedArray = substr($resultString,0,-1);
+        $key = implode('', $result); // Convert the $result array into a string
+        return hash_hmac('sha256', $SortedArray, $key);
+    }
+    /**
      * Create Hash Array
      */
-
-    public function HashArray($data)
+    public function HashArray($data): string
     {
         $result = [];
         foreach ($data as $key => $value) {
             $result[] = $value;
         }
-
         $resultString = implode('', $result);
+
         $SortedArray = '';
-        for($i = 0; $i < strlen($resultString); $i++) {
+
+        for ($i = 0; $i < strlen($resultString); $i++) {
             if ($resultString[$i] != 'undefined' and $resultString[$i] != null and $resultString[$i] != "") {
                 $SortedArray .= "&" . $resultString[$i];
             }
@@ -98,18 +149,19 @@ class JazzCash
      * Generate the HTML to render in Mobile Application to send Request:
      *
      */
-    public function renderPage($data) {
-        $jazzcashForm[] = '<div id="header"><form action="' . $this->apiUrl . '" method="post" id="jazzcash-checkout">';
+    public function renderPage($data): string
+    {
+        $jazzcashForm[] = '<div id="header"><form id="jc-params" action="' . $this->apiUrl . '" method="post" id="jazzcash-checkout">';
 
         foreach ($data as $key => $value) {
             $jazzcashForm[] = '<input type="text" name="' . ($key) . '" id="' . ($key) . '" value="' . ($value) . '" />';
         }
         $jazzcashForm[] = '<input type="submit" class="button paydast-submit" name="" value="Submit" />';
-
-        $jazzcashForm[] = '</form></div>';
+        $jazzcashForm[] = '<script> window.addEventListener("load", function() {    document.getElementById("myForm").submit();  });</script></form></div>';
 
         return implode('', $jazzcashForm);
     }
+
     /**
      * Set Amount for Orders
      *
@@ -124,7 +176,7 @@ class JazzCash
      * Get the amount for Order
      * @return int;
      */
-    public function getAmount():int
+    public function getAmount(): int
     {
         return $this->amount;
     }
@@ -133,7 +185,7 @@ class JazzCash
      * Set Bill Reference for Jazz Cash Order
      *
      */
-    public function setBillReference($billref)
+    public function setBillReference($billref): static
     {
         $this->billreference = $billref;
         return $this;
@@ -143,7 +195,8 @@ class JazzCash
      * Get the Bill Reference Number for Jazz Cash Order
      *
      */
-    public function getBillRefernce() {
+    public function getBillRefernce()
+    {
         return $this->billreference;
     }
 
@@ -162,7 +215,8 @@ class JazzCash
      * Get the Product Description for Jazz Cash Order
      *
      */
-    public function getProductDescription() {
+    public function getProductDescription()
+    {
         return $this->billreference;
     }
 
@@ -176,5 +230,54 @@ class JazzCash
     {
         $this->apiUrl = $apiUrl;
         return $this;
+    }
+
+    /**
+     *
+     * @return mixed
+     */
+    public function getApiUrl(): mixed
+    {
+        return $this->apiUrl;
+    }
+
+    /**
+     * Set the value for Refund API Url
+     *
+     * @param $apiUrl
+     * @return  self
+     */
+    public function setRefundApiUrl($apiUrl): static
+    {
+        $this->refundURL = $apiUrl;
+        return $this;
+    }
+
+    /**
+     * Get the Refund API Url
+     * @return mixed
+     */
+    public function getRefundApiUrl(): mixed
+    {
+        return $this->refundURL;
+    }
+
+    /**
+     * Set the value Mobile Pin
+     *
+     */
+    public function SetMpin($mpin)
+    {
+        $this->mpin = $mpin;
+        return $this;
+    }
+
+    /**
+     * Get the MPIN
+     * @return mixed
+     */
+    public function GetMpin(): mixed
+    {
+        return $this->mpin;
     }
 }
